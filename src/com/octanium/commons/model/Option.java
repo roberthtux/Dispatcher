ed08626425bfa2742dc093b091e0975f5c2033ab
roberthtux <roberthtux@gmail.com>
2016-08-21 20:54:58 -0500

    Add Pojos
    Se agregan los pojos Option y Profile

---
+++ src/com/octanium/commons/model/Option.java
@@ -0,0 +1,40 @@
+package com.octanium.commons.model;
+
+public class Option {
+private int id;
+private String name;
+private String buttonId;
+private String url;
+private String function;
+
+public int getId() {
+	return id;
+}
+public String getName() {
+	return name;
+}
+public String getButtonId() {
+	return buttonId;
+}
+public String getUrl() {
+	return url;
+}
+public String getFunction() {
+	return function;
+}
+public void setId(int id) {
+	this.id = id;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public void setButtonId(String buttonId) {
+	this.buttonId = buttonId;
+}
+public void setUrl(String url) {
+	this.url = url;
+}
+public void setFunction(String function) {
+	this.function = function;
+}
+}
---
+++ src/com/octanium/commons/model/Profile.java
@@ -0,0 +1,20 @@
+package com.octanium.commons.model;
+
+public class Profile {
+	private int id;
+	private String name;
+	
+	public int getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
