03ce240289ff011644f8915da018cf51af60c3d3
roberthtux <roberthtux@gmail.com>
2016-08-30 17:22:18 -0500

    Add DateDeserializer
    Se agrega la grase DateDeserializer para fechas

---
+++ src/com/octanium/commons/utils/DateDeserializer.java
@@ -0,0 +1,30 @@
+package com.octanium.commons.utils;
+
+import java.lang.reflect.Type;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
+
+public class DateDeserializer implements JsonDeserializer<Date> {
+
+	@Override
+	public Date deserialize(JsonElement element, Type arg1, JsonDeserializationContext arg2) throws JsonParseException {
+		String date = element.getAsString();
+		
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		formatter.setTimeZone(TimeZone.getTimeZone("UTC"));
+		
+		try {
+			return formatter.parse(date);
+		} catch (ParseException e) {
+			System.err.println(e);
+			return null;
+		}
+	}
+}
