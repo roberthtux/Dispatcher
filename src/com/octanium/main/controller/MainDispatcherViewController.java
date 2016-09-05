16acd63586c89174430fcb715036401f9c71ff89
roberthtux <roberthtux@gmail.com>
2016-09-05 08:20:49 -0500

    Modify
    Se modifican los archivos Main y Registro

--- src/com/octanium/main/controller/MainDispatcherViewController.java
+++ src/com/octanium/main/controller/MainDispatcherViewController.java
@@ -27,96 +27,105 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 
 public class MainDispatcherViewController {
-    @FXML
-    private Button btnMenuBar;
-    @FXML
-    private Button btnRegistro;
-    @FXML
-    private Button btnHorarios;
-    @FXML
-    private Button btnApertura;
-    @FXML
-    private Button btnDepositos;
-    @FXML
-    private Button btnFacturacion;
-    @FXML
-    private Button btnTickets;
-    @FXML
-    private Button btnCierre;
-    @FXML
-    private Button btnCambioNip;
-    @FXML
-    private VBox dataPane;
-    @FXML
-    private Button logoutButton;
-    @FXML
-    private Label sessionLabel;
+	@FXML
+	private Button btnMenuBar;
+	@FXML
+	private Button btnRegistro;
+	@FXML
+	private Button btnHorarios;
+	@FXML
+	private Button btnApertura;
+	@FXML
+	private Button btnDepositos;
+	@FXML
+	private Button btnFacturacion;
+	@FXML
+	private Button btnTickets;
+	@FXML
+	private Button btnCierre;
+	@FXML
+	private Button btnCambioNip;
+	@FXML
+	private VBox dataPane;
+	@FXML
+	private Button logoutButton;
+	@FXML
+	private Label sessionLabel;
+@FXML
+private AnchorPane paneButtons;
+	
+	private User user;
 
-    private User user;
-    
-    public void setDataPane(Node node) {
-        // update VBox with new form(FXML) depends on which button is clicked
-        dataPane.getChildren().setAll(node);
-    }
+	public void setDataPane(Node node) {
+		// update VBox with new form(FXML) depends on which button is clicked
+		dataPane.getChildren().setAll(node);
+	}
 
-    public VBox fadeAnimate(String url) throws IOException {
-        VBox v = (VBox) FXMLLoader.load(getClass().getResource(url));
-      /*  FadeTransition ft = new FadeTransition(Duration.millis(1500));
+	public VBox fadeAnimate(String url) throws IOException {
+		VBox v = (VBox) FXMLLoader.load(getClass().getResource(url));
+		/*  FadeTransition ft = new FadeTransition(Duration.millis(1500));
         ft.setNode(v);
         ft.setFromValue(0.1);
         ft.setToValue(1);
         ft.setCycleCount(1);
         ft.setAutoReverse(false);
         ft.play();*/
-        return v;
-    }
+		return v;
+	}
 
-    public void loadPane(ActionEvent event) throws IOException {
-//    	FXMLLoader loader = new FXMLLoader(
-//	    		  Thread.currentThread().getContextClassLoader().getResource("com/octanium/main/view/Registro.fxml")
-//	      );
-    	
-    	setDataPane(fadeAnimate("/view/FXML1.fxml"));
-    }
+	public void loadPane(ActionEvent event) throws IOException {
+		//    	FXMLLoader loader = new FXMLLoader(
+		//	    		  Thread.currentThread().getContextClassLoader().getResource("com/octanium/main/view/Registro.fxml")
+		//	      );
 
-    public void loadRegistro(ActionEvent event) throws IOException {
-    	setDataPane(fadeAnimate("/com/octanium/main/view/Registro.fxml"));
-    }
+		setDataPane(fadeAnimate("/view/FXML1.fxml"));
+	}
 
-    public void loadHorarios(ActionEvent event) throws IOException {
-        setDataPane(fadeAnimate("/com/octanium/main/view/Horarios.fxml"));
-    }
-    public void loadApertura(ActionEvent event) throws IOException {
-    	setDataPane(fadeAnimate("/com/octanium/main/view/Apertura.fxml"));
-    }
-    
-    public void initialize() {
-    }
-	  
-	  public void initSessionID(final LoginManager loginManager, User user) {
-		  this.user=user;
-	    sessionLabel.setText(user.getName() +" "+user.getFirstLastname() +" "+ user.getSecondLastname());
-	    
-	    ClientConfig config = new ClientConfig();
-    	Client client = ClientBuilder.newClient(config);
-    	URI uri = UriBuilder.fromUri(Constants.URI).build();
-    	WebTarget target = client.target(uri);
-    	
-    	String plainAnswer = 
-    			target.path("profile")
-    			.path(this.user.getId().toString())
-    			.request().accept(MediaType.APPLICATION_JSON).get(String.class);
+	public void loadRegistro(ActionEvent event) throws IOException {
+		setDataPane(fadeAnimate("/com/octanium/main/view/Registro.fxml"));
+	}
 
-    	Gson gson = new Gson();
-    	List<Map<String, String>> mapProfile = gson.fromJson(plainAnswer, new TypeToken<List<Map<String, Object>>>() {}.getType());
-	    
-	    logoutButton.setOnAction(new EventHandler<ActionEvent>() {
-	      @Override public void handle(ActionEvent event) {
-	        loginManager.logout();
-	      }
-	    });
-	  }
+	public void loadHorarios(ActionEvent event) throws IOException {
+		setDataPane(fadeAnimate("/com/octanium/main/view/Horarios.fxml"));
+	}
+	public void loadApertura(ActionEvent event) throws IOException {
+		setDataPane(fadeAnimate("/com/octanium/main/view/Apertura.fxml"));
+	}
+
+	public void initialize() {
+	}
+
+	public void initSessionID(final LoginManager loginManager, User user) {
+		this.user=user;
+		sessionLabel.setText(user.getName() +" "+user.getFirstLastname() +" "+ user.getSecondLastname());
+
+		ClientConfig config = new ClientConfig();
+		Client client = ClientBuilder.newClient(config);
+		URI uri = UriBuilder.fromUri(Constants.URI).build();
+		WebTarget target = client.target(uri);
+
+		String plainAnswer = 
+				target.path("profile")
+				.path(this.user.getId().toString())
+				.request().accept(MediaType.APPLICATION_JSON).get(String.class);
+
+		Gson gson = new Gson();
+		List<HashMap<String, String>> mapOptions = gson.fromJson(plainAnswer, new TypeToken<List<HashMap<String, Object>>>() {}.getType());
+		Double topAnchor=0.0;
+		for(HashMap<String, String> option: mapOptions){
+			Button bt=(Button) paneButtons.lookup("#"+option.get("buttonId"));
+			bt.setVisible(true);
+			AnchorPane.setTopAnchor(bt, topAnchor);
+			topAnchor+=30.0;
+		}
+		logoutButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override public void handle(ActionEvent event) {
+				loginManager.logout();
+			}
+		});
+	}
 }
